dc212ec0e82dc5fd280d79b21dd3ab1c3ae333d5
aliv-tradedoubler <aliaksandr.ivanou@tradedoubler.com>
2017-08-01 15:53:25 +0200

    Added disk level suite

--- core/src/test/scala/org/cddb/lsmt/DiskLevelSuite.scala
+++ core/src/test/scala/org/cddb/lsmt/DiskLevelSuite.scala
@@ -1,96 +1,133 @@
 package org.cddb.lsmt
 
-import java.util.Random
 
-import org.cddb.io.{Config, DiskBlock}
-import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
+class DiskLevelSuite extends TableSuite with DataConsistency {
 
-
-@RunWith(classOf[JUnitRunner])
-class DiskLevelSuite extends FunSuite {
-
-  val config = Config("/Users/aliaksandrivanou/temp/data", "index.dat")
-
-  val rand = new Random(System.nanoTime())
-
-  test("dl scatter test") {
+  test("dl single table insert should produce index with size=1") {
     val tableManager = SSTableManager()
     val indexHandler = new DataIndexHandler(config)
-    var dl = new DiskLevel(tableManager, indexHandler)
-    val t1 = randomSSTable(500)
-    val t2 = randomSSTable(300)
-    val t3 = randomSSTable(300)
-    val t4 = randomSSTable(300)
+    val dl = new DiskLevel(config, tableManager, indexHandler)
+    dl.scatter(randomSSTable(100, randomRec))
+    val ind = indexHandler.readIndex()
+    checkDataConsistency(ind)
+    assertResult(ind.size)(1)
+    val optSSTable = ind.iterator.next._2.load()
+    assert(optSSTable.isDefined)
+    checkSSTableConsistency(optSSTable.get)
+    dl.destroy()
+  }
+
+  test("dl single table insert should produce index with size=2 if sstable has more els than maxTableSize") {
+    val tableManager = SSTableManager()
+    val maxTableSize = 100
+    val indexHandler = new DataIndexHandler(config)
+    val dl = new DiskLevel(config, tableManager, indexHandler, maxTableSize)
+    dl.scatter(randomSSTable(250, randomRec))
+    val ind = indexHandler.readIndex()
+    checkDataConsistency(ind)
+    assertResult(4)(ind.size)
+    val it = indexHandler.readIndex().iterator
+    while (it.nonEmpty) {
+      val (range, db) = it.next
+      checkRangeConsistency(range)
+      val optTable = db.load()
+      assert(optTable.isDefined)
+      checkSSTableConsistency(optTable.get)
+    }
+  }
+
+  test("dl update should not create additional blocks if the size is smaller than MAX_SIZE") {
+    val tableManager = SSTableManager()
+    val maxTableSize = 1000
+    val indexHandler = new DataIndexHandler(config)
+    val dl = new DiskLevel(config, tableManager, indexHandler, maxTableSize)
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    val ind = indexHandler.readIndex()
+    checkDataConsistency(ind)
+    assertResult(1)(ind.size)
+  }
+
+  test("dl split should occur if records greater than MAX_SIZE") {
+    val tableManager = SSTableManager()
+    val maxTableSize = 1000
+    val indexHandler = new DataIndexHandler(config)
+    val dl = new DiskLevel(config, tableManager, indexHandler, maxTableSize)
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    dl.scatter(randomSSTable(250, randomRec))
+    val ind = indexHandler.readIndex()
+    checkDataConsistency(ind)
+    assertResult(2)(ind.size)
+  }
+
+  test("dl scatter append/read operations (not unique vals)") {
+    val tableManager = SSTableManager()
+    val indexHandler = new DataIndexHandler(config)
+    var dl = new DiskLevel(config, tableManager, indexHandler)
+    val t1 = randomSSTable(1000, simRandomRec)
+    val t2 = randomSSTable(3000, simRandomRec)
+    val t3 = randomSSTable(3000, simRandomRec)
+    val t4 = randomSSTable(3000, simRandomRec)
     dl.scatter(t1)
     dl.scatter(t2)
     dl.scatter(t3)
     dl.scatter(t4)
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(1000))
-    dl.scatter(randomSSTable(10000))
-
     val index = indexHandler.readIndex()
     checkDataConsistency(index)
     dl.cleanAndClose()
+    dl = new DiskLevel(config, tableManager, new DataIndexHandler(config))
+    checkExistenceOrUpdate(dl, t1)
+    checkExistenceOrUpdate(dl, t2)
+    checkExistenceOrUpdate(dl, t3)
+    checkExistenceOrUpdate(dl, t4)
     dl.destroy()
   }
 
-  def checkDataConsistency(ind: scala.collection.immutable.TreeMap[Range, DiskBlock[SSTable]]): Unit = {
-    val it = ind.iterator
-    if (it.isEmpty) return
-    var prev = it.next
-    checkSSTableConsistency(prev._2.load())
-    while (it.nonEmpty) {
-      val (currRange, currBlock) = it.next()
-      currBlock.load() match {
-        case Some(table) =>
-          checkSSTableConsistency(table)
-        case None =>
-      }
-      val prevRange = prev._1
-      compareRanges(prevRange, currRange)
-      prev = (currRange, currBlock)
+  def checkExistenceOrUpdate(dl: DiskLevel, table: SSTable): Unit = {
+    for (rec <- table.records) {
+      val mbRec = dl.read(rec.key)
+      assert(mbRec.isDefined)
+      val dbRec = mbRec.get
+      assert(dbRec.timestamp >= rec.timestamp)
     }
   }
 
-  def compareRanges(low: Range, high: Range): Unit = {
-    assert(checkRangeConsistency(low))
-    assert(checkRangeConsistency(high))
-    assert(high.from.compareTo(low.to) > 0)
+  test("dl scatter append/read operations (unique vals)") {
+    val tableManager = SSTableManager()
+    val indexHandler = new DataIndexHandler(config)
+    var dl = new DiskLevel(config, tableManager, indexHandler)
+    val t1 = randomSSTable(1000, randomUniqueRec)
+    val t2 = randomSSTable(3000, randomUniqueRec)
+    val t3 = randomSSTable(3000, randomUniqueRec)
+    val t4 = randomSSTable(3000, randomUniqueRec)
+    dl.scatter(t1)
+    dl.scatter(t2)
+    dl.scatter(t3)
+    dl.scatter(t4)
+    val index = indexHandler.readIndex()
+    checkDataConsistency(index)
+    dl.cleanAndClose()
+    dl = new DiskLevel(config, tableManager, new DataIndexHandler(config))
+    checkExistence(dl, t1)
+    checkExistence(dl, t2)
+    checkExistence(dl, t3)
+    checkExistence(dl, t4)
+    dl.destroy()
   }
 
-  def checkRangeConsistency(r: Range): Boolean = r.to.compareTo(r.from) >= 0
-
-  def checkSSTableConsistency(table: Option[SSTable]): Unit = table match {
-    case Some(t) => checkSSTableConsistency(t)
-    case None =>
-  }
-
-  def checkSSTableConsistency(table: SSTable): Unit = {
-    if (table.size == 0) return
-    for (i <- 1 until table.size) {
-      val (prev, curr) = (table.get(i - 1), table.get(i))
-      assert(curr.key.compareTo(prev.key) > 0)
+  def checkExistence(dl: DiskLevel, table: SSTable): Unit = {
+    for (rec <- table.records) {
+      val mbRec = dl.read(rec.key)
+      assert(mbRec.isDefined)
+      val dbRec = mbRec.get
+      assertResult(rec.value)(dbRec.value)
+      assertResult(rec.timestamp)(dbRec.timestamp)
     }
   }
 
-  def randomSSTable(size: Int) = randomTreeTable(size).persistPart
-
-  def randomTreeTable(size: Int) = {
-    val tt = TreeTable(TableMetadata(size, 1.0, size))
-    (0 until size).foreach(_ => tt.append(randomRec()))
-    tt
-  }
-
-  def randomRec(): Record = Record(rstring(), rstring(), rstring(), System.nanoTime())
-
-  def rstring(): String = rand.nextInt(2000).toString
 
 }
